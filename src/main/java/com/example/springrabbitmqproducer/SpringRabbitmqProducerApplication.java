ee5ab2cbd1b0aef52ad6a20ab27cb0746609aaae
Bharath K3 <bharath.k3@ibm.com>
2024-03-21 17:24:06 +0530

    secon mmmm

--- src/main/java/com/example/springrabbitmqproducer/SpringRabbitmqProducerApplication.java
+++ src/main/java/com/example/springrabbitmqproducer/SpringRabbitmqProducerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRabbitmqProducerApplication {
 
 	public static void main(String[] args) {
+		System.out.print("application started succfully");
 		SpringApplication.run(SpringRabbitmqProducerApplication.class, args);
 	}
 
